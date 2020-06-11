6e0cfac3f44242b6e3bcd46e94627737e52473a1
roushan <roushan97@gmail.com>
2020-06-11 15:35:26 +0800

    Updates

---
+++ precache-manifest.7dd87dddc94a6b346577bf13abd58279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873051a8c23916a04f92fd89b32b50dc",
+    "url": "/react-mailingproj/index.html"
+  },
+  {
+    "revision": "384357556654ccaf80fb",
+    "url": "/react-mailingproj/static/css/main.72d4e965.chunk.css"
+  },
+  {
+    "revision": "f124300e3d0375b354a8",
+    "url": "/react-mailingproj/static/js/2.8628139c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mailingproj/static/js/2.8628139c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384357556654ccaf80fb",
+    "url": "/react-mailingproj/static/js/main.3e151b09.chunk.js"
+  },
+  {
+    "revision": "7cd7b6c288bf2013deeb",
+    "url": "/react-mailingproj/static/js/runtime-main.06c27bac.js"
+  }
+]);
